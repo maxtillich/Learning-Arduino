f903365a6fe1bcef0d5ce9244ad6017d16e200ae
Max Tillich <mtillich@gmail.com>
2013-05-06 00:21:06 +0200

    added counter and really basic morse representation w/ x and o

--- Digital_Read_Serial/Digital_Read_Serial.ino
+++ Digital_Read_Serial/Digital_Read_Serial.ino
@@ -1,9 +1,24 @@
+int lastValue;
+int counter;
+int longThreshold = 3;
 void setup() {
   Serial.begin(9600); // bits/sec
   pinMode(2,INPUT); // Pin 2 als input definieren
+  counter = 0;
 }
 
 void loop() {
   int inputValue = digitalRead(2);
-  Serial.println(inputValue);
+  if (inputValue != lastValue) {
+    if (lastValue == 1) {
+      if (counter > longThreshold)
+        Serial.println('X');
+      else
+        Serial.println('O');
+    }
+    counter = 0;
+  }
+  counter ++;
+  lastValue = inputValue;
+  delay(50);
 }
